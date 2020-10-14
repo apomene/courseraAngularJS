09df67a511d6a03f3b8167cba1bef02897a0564a
Apo Mene <apomene@yahoo.gr>
2020-10-14 13:21:16 +0300

    Finished solution

--- Mode1-Solution/app.js
+++ Mode1-Solution/app.js
@@ -1,24 +1,33 @@
 (function () {
 'use strict';
-
 angular.module('module1App', [])
-.controller('module1AppController', LunchCheckController);
+.controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
+
 function LunchCheckController($scope) {
- var numOfdishes = countDishes($scope.dishes);
- if (numOfdishes == 0){
-   $scope.message = "Please enter data first";
- }
- else if(numOfdishes<=3){
-  $scope.message = "Enjoy!";
- }
- else{
-  $scope.message = "Too much!";
- }
+    $scope.message="";
+    $scope.dishes="";
+    //$scope.dishes="1,2,3,4,5,6,7,8";
+
+  $scope.Check = function (){
+    var numOfdishes = countDishes($scope.dishes);
+    if (numOfdishes == 0){
+      $scope.message = "Please enter data first";
+    }
+    else if(numOfdishes<=3){
+    $scope.message = "Enjoy!";
+    }
+    else{
+    $scope.message = "Too much!";
+    }
+  }
 }
 
 function countDishes(dishes){
+  if (dishes =='' || dishes == {}){
+    return 0;
+  }
   return dishes.split(',').length;
 }
 
